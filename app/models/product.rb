17c8091357807fdb9ec72868f0b4f5613b57c120
Avinash Sarguru <avinashsarguru@gmail.com>
2020-11-05 22:36:31 -0600

    class template set up

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,19 @@
-class Product < ApplicationRecord
+class ProductAvailability < Trailblazer::Operation
+  step :read_params
+  step :fetch_product_and_location_details
+  step :calculate_availability
+  step :persist_and_build_output
+  def read_params(options)
+    # logic to read the product and location id params passed 
+      #if params not passed, return false (this will exit the concept and return to the caller)
+  end
+  def fetch_product_and_location_details(options)
+    # hit db/cache to fetch location and product details
+  end
+  def calculate_availability(options)
+    # logic to calculate availability for a product-location
+  end
+  def persist_and_build_output(options) 
+    # store the availability calculated and return the response
+  end 
 end
